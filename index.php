946017ee2d1e328aa9320fc77bef68164acb082e
joris <jorisoptenberg@ictindewolken.nl>
2017-10-02 13:44:16 +0200

    index is now a php file

---
+++ index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE HTML>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>
+      My Website
+    </title>
+    <link rel="stylesheet" href="CSS/style.css">
+ </head>
+  <center>
+  <body>
+    <header>
+      [Logo] My Website
+    </header>
+
+    <div id="menu">
+      <ul>
+        <li><a href="index.html">Home</a></li>
+        <li><a href="#">menuitem 2</a></li>
+        <li><a href="#">menuitem 3</a></li>
+        <li><a href="#">menuitem 4</a></li>
+      </ul>
+    </div>
+
+    <div id="content">
+    	<div>
+        [article 1]
+      </div>
+
+     	<div>
+        [article 2]
+      </div>
+
+      <div>
+       	[article 3]
+      </div>
+    </div>
+
+    <div id="footer">
+      &copy; Joris op ten Berg <?php echo "Today is " . date("Y/m/d")?>
+    </div>
+
+  </body>
+</center>
+</html>
